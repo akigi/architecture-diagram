74994b64ff103769455891dd103805429953ceff
akigi <121186452+akigi@users.noreply.github.com>
2023-01-16 03:45:05 +0900

    update

--- mygraph.js
+++ mygraph.js
@@ -9,6 +9,7 @@ function makeSVG(addGridLines, mywidth, myheight) {
 function gridify(svg, pgLayout, margin, groupMargin) {
     // routes
     var routes = cola.gridify(pgLayout, 5, margin, groupMargin);
+    console.log("routes", routes)
     svg.selectAll('path').remove();
 
     // forEach
@@ -41,14 +42,14 @@ function gridify(svg, pgLayout, margin, groupMargin) {
             var h = this.getBBox().height;
             return d.bounds.cy() + h / 3.5;
         });
-    
+
     // node
     svg.selectAll(".node").transition().attr("x", d => d.routerNode.bounds.x)
         .attr("y", d => d.routerNode.bounds.y)
         .attr("width", d => d.routerNode.bounds.width())
         .attr("height", d => d.routerNode.bounds.height());
-    
-    
+
+
     // group
     let groupPadding = margin - groupMargin;
     svg.selectAll(".group").transition().attr('x', d => d.routerNode.bounds.x - groupPadding)
@@ -63,7 +64,7 @@ function createPowerGraph(inputjson) {
     var grouppadding = 0.01;
     var margin = 20;
     var groupMargin = 15;
-    
+
     // svg
     var svg = makeSVG(false, size[0], size[1]);
 
@@ -75,12 +76,12 @@ function createPowerGraph(inputjson) {
 
     // pgLayout
     var pgLayout = cola.powerGraphGridLayout(inputjson, size, grouppadding);
-    pgLayout.cola._nodes[0].x = 0
-    
+    // pgLayout.cola._nodes[0].x = 0
+
     // debug
     console.log("inputjson", inputjson)
     console.log("pgLayout", pgLayout)
-    
+
     // filter duplicate links:
     //var es = pgLayout.powerGraph.powerEdges;
     //var copy = [];
@@ -97,27 +98,27 @@ function createPowerGraph(inputjson) {
     //    if (!dupFound) copy.push(e);
     //}
     //pgLayout.powerGraph.powerEdges = copy;
-    
+
     // group
     var group = svg.selectAll(".group")
         .data(pgLayout.powerGraph.groups)
         .enter().append("rect")
         .attr("class", "group");
-    
+
     // node
     var node = svg.selectAll(".node")
         .data(inputjson.nodes)
         .enter().append("rect")
         .attr("class", "node");
     node.append("title").text(d => d.name);
-    
+
     // label
     var label = svg.selectAll(".label")
         .data(inputjson.nodes)
         .enter().append("text")
         .attr("class", "label")
         .text(d => d.name.replace(/^u/, ''));
-    
+
     // gridify
     gridify(svg, pgLayout, margin, groupMargin);
 
@@ -172,7 +173,7 @@ function createPowerGraph(inputjson) {
         .on("dragstart", dragStart)
         .on("drag", drag)
         .on("dragend", dragEnd);
-    
+
     // call
     node.call(dragListener);
     label.call(dragListener);
@@ -180,20 +181,24 @@ function createPowerGraph(inputjson) {
 
 // Do
 a = d3.text("graphdata.dot", function (f) {
+    // f
+    // console.log("f", f)
+
     // digraph
     var digraph = graphlibDot.read(f);
-    
+
     console.log("digraph", digraph)
 
     // nodes
     var nodeNames = digraph.nodes();
-    console.log("digraph.nodes()",digraph.nodes())
+    console.log("digraph.nodes()", digraph.nodes())
     var nodes = new Array(nodeNames.length);
     nodeNames.forEach(function (name, i) {
         var v = nodes[i] = digraph._nodes[nodeNames[i]];
         v.id = i;
         v.name = name;
     });
+    console.log("nodes[0]", nodes[0])
 
     // edges
     const dedges = (digraph.edges());
@@ -205,6 +210,8 @@ a = d3.text("graphdata.dot", function (f) {
             target: digraph._nodes[edge.w].id
         });
     }
+    console.log("edges[0]", edges[0])
+    console.log("edges[0].source", edges[0].source)
 
     // createPowerGraph
     let arg = {
@@ -214,4 +221,148 @@ a = d3.text("graphdata.dot", function (f) {
     console.log("arg", arg);
     createPowerGraph(arg);
 });
+
+
+
+// https://127.0.0.1:4001
+
+
+// var graph = {
+//     nodes: [   
+//         {id: 0, name: "node0", width: 70, height: 70},
+//         {id: 1, name: "node1", width: 70, height: 70},
+//         {id: 2, name: "node2", width: 70, height: 70},
+//         {id: 3, name: "node3", width: 70, height: 70},
+//     ],
+//     links: [
+//         {source: 0, target: 1},
+//         {source: 0, target: 2},
+//         {source: 3, target: 0},
+//     ]
+// }
+
+var G = `digraph G {
+node1[label="node 1"];
+node2[label="node 2"];
+node3[label="node 3"];
+node4[label="node 4"];
+node1 -> node2[label="()"];
+node2 -> node4[label="()"];
+node2 -> node3[label="()"];
+node2 -> node1[label="()"];
+node1 -> node2[label="()"];
+}
+`;
+
+var digraph = graphlibDot.read(G);
+console.log("digraph", digraph);
+
+var graph = {
+    nodes: [],
+    links: []
+}
+
+digraph.nodes().forEach((nodeName, i) => {
+    let node = digraph._nodes[nodeName];
+    node.id = i;
+    node.name = nodeName;
+    node.width = 70;
+    node.height = 70;
+    graph.nodes.push(node);
+});
+
+
+for (let edge of digraph.edges()) {
+    graph.links.push({
+        source: digraph._nodes[edge.v].id,
+        target: digraph._nodes[edge.w].id
+    });
+}
+console.log("graph", graph);
+
+var size = [700, 700];
+var grouppadding = 0.01;
+var margin = 20;
+var groupMargin = 15;
+
+
+var pgLayout = cola.powerGraphGridLayout(graph, size, grouppadding);
+console.log("pgLayout", pgLayout)
+// pgLayout.powerGraph.groups = [];
+
+pgLayout.powerGraph.powerEdges = [];
+
+pgLayout.powerGraph.powerEdges.push(
+    {
+        source: graph.nodes[3],
+        target: graph.nodes[1],
+        type: 0
+    },
+    {
+        source: graph.nodes[0],
+        target: graph.nodes[1],
+        type: 0
+    }
+    
+)
+
+var routes = cola.gridify(pgLayout, 5, margin, groupMargin);
+console.log("routes", routes);
+
+// svg
+var svg = d3.select("body").append("svg")
+    .attr("width", size[0])
+    .attr("height", size[1]);
+
+var node = svg.selectAll(".node")
+    .data(graph.nodes)
+    .enter().append("rect")
+    .attr("x", d => d.routerNode.bounds.x)
+    .attr("y", d => d.routerNode.bounds.y)
+    .attr("width", d => d.routerNode.bounds.width())
+    .attr("height", d => d.routerNode.bounds.height())
+    .attr("class", "node")
+    .attr("fill", "black")
+
+routes.forEach(route => {
+    var cornerradius = 5;
+    var arrowwidth = 3;
+    var arrowheight = 7;
+    var p = cola.GridRouter.getRoutePath(route, cornerradius, arrowwidth, arrowheight);
+    // console.log("p", p)
+    svg.append("path")
+        .attr("class", "link")
+        .attr("d", p.routepath)
+        .attr("fill", "none")
+        .attr("stroke", "black")
+        .attr("stroke-width", 2);
+    svg.append("path")
+        .attr("class", "linkarrow")
+        .attr("d", p.arrowpath)
+        .attr("stroke", "black")
+        .attr("stroke-width", 2);    
+});
+
+
+
+
+// group
+var group = svg.selectAll(".group")
+    .data(pgLayout.powerGraph.groups)
+    .enter().append("rect")
+    .attr("class", "group");
+
+// node
+var node = svg.selectAll(".node")
+    .data(graph.nodes)
+    .enter().append("rect")
+    .attr("class", "node");
+node.append("title").text(d => d.name);
+
+// label
+var label = svg.selectAll(".label")
+    .data(graph.nodes)
+    .enter().append("text")
+    .attr("class", "label")
+    .text(d => d.name.replace(/^u/, ''));
 
