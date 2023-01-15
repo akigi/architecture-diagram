ab00ccbd8841fe3f68981fb02e5cf0f440bf2ed2
akigi <121186452+akigi@users.noreply.github.com>
2023-01-15 13:14:12 +0900

    update

--- mygraph.js
+++ mygraph.js
@@ -1,4 +1,3 @@
-
 function makeSVG(addGridLines, mywidth, myheight) {
     var svg = d3.select("body").append("svg")
         .attr("width", mywidth)
@@ -6,9 +5,13 @@ function makeSVG(addGridLines, mywidth, myheight) {
     return svg;
 }
 
+
 function gridify(svg, pgLayout, margin, groupMargin) {
+    // routes
     var routes = cola.gridify(pgLayout, 5, margin, groupMargin);
     svg.selectAll('path').remove();
+
+    // forEach
     routes.forEach(route => {
         var cornerradius = 5;
         var arrowwidth = 3;
@@ -31,15 +34,22 @@ function gridify(svg, pgLayout, margin, groupMargin) {
             .attr('d', p.routepath)
             .attr('fill', 'none');
     });
+
+    // label
     svg.selectAll(".label").transition().attr("x", d => d.routerNode.bounds.cx())
         .attr("y", function (d) {
             var h = this.getBBox().height;
             return d.bounds.cy() + h / 3.5;
         });
+    
+    // node
     svg.selectAll(".node").transition().attr("x", d => d.routerNode.bounds.x)
         .attr("y", d => d.routerNode.bounds.y)
         .attr("width", d => d.routerNode.bounds.width())
         .attr("height", d => d.routerNode.bounds.height());
+    
+    
+    // group
     let groupPadding = margin - groupMargin;
     svg.selectAll(".group").transition().attr('x', d => d.routerNode.bounds.x - groupPadding)
         .attr('y', d => d.routerNode.bounds.y + 2 * groupPadding)
@@ -49,17 +59,26 @@ function gridify(svg, pgLayout, margin, groupMargin) {
 
 
 function createPowerGraph(inputjson) {
+    // svg
     var size = [700, 700];
     var svg = makeSVG(false, size[0], size[1]);
     var grouppadding = 0.01;
+    var margin = 20;
+    var groupMargin = 15;
+    
+    // inputJson.nodes
     inputjson.nodes.forEach(v => {
         v.width = 70;
         v.height = 70;
     });
-    console.log(inputjson)
-    var margin = 20;
-    var groupMargin = 15;
+
+    // pgLayout
     var pgLayout = cola.powerGraphGridLayout(inputjson, size, grouppadding);
+    
+    // debug
+    console.log("inputjson", inputjson)
+    console.log("pgLayout", pgLayout)
+    
     // filter duplicate links:
     //var es = pgLayout.powerGraph.powerEdges;
     //var copy = [];
@@ -76,24 +95,34 @@ function createPowerGraph(inputjson) {
     //    if (!dupFound) copy.push(e);
     //}
     //pgLayout.powerGraph.powerEdges = copy;
+    
+    // group
     var group = svg.selectAll(".group")
         .data(pgLayout.powerGraph.groups)
         .enter().append("rect")
         .attr("class", "group");
+    
+    
+    // node
     var node = svg.selectAll(".node")
         .data(inputjson.nodes)
         .enter().append("rect")
         .attr("class", "node");
     node.append("title").text(d => d.name);
+    
+    // label
     var label = svg.selectAll(".label")
         .data(inputjson.nodes)
         .enter().append("text")
         .attr("class", "label")
         .text(d => d.name.replace(/^u/, ''));
+    
+    // gridify
     gridify(svg, pgLayout, margin, groupMargin);
+
+    // Events
     let eventStart = {},
         ghosts = null;
-
     function getEventPos() {
         let ev = d3.event;
         let e = typeof TouchEvent !== 'undefined' && ev.sourceEvent instanceof TouchEvent ? (ev.sourceEvent).changedTouches[0] : ev.sourceEvent;
@@ -102,7 +131,6 @@ function createPowerGraph(inputjson) {
             y: e.clientY
         };
     }
-
     function dragStart(d) {
         ghosts = [1, 2].map(i => svg.append('rect')
             .attr({
@@ -114,7 +142,6 @@ function createPowerGraph(inputjson) {
             }));
         eventStart[d.routerNode.id] = getEventPos();
     }
-
     function getDragPos(d) {
         let p = getEventPos(),
             startPos = eventStart[d.routerNode.id];
@@ -123,12 +150,10 @@ function createPowerGraph(inputjson) {
             y: d.routerNode.bounds.y + p.y - startPos.y
         };
     }
-
     function drag(d) {
         var p = getDragPos(d);
         ghosts[1].attr(p);
     }
-
     function dragEnd(d) {
         let dropPos = getDragPos(d);
         delete eventStart[d.routerNode.id];
@@ -139,16 +164,23 @@ function createPowerGraph(inputjson) {
             gridify(svg, pgLayout, margin, groupMargin);
         }
     }
+
+    // Events Listener
     let dragListener = d3.behavior.drag()
         .on("dragstart", dragStart)
         .on("drag", drag)
         .on("dragend", dragEnd);
+    
+    // call
     node.call(dragListener);
     label.call(dragListener);
 }
 
+// Do
 a = d3.text("graphdata.dot", function (f) {
     var digraph = graphlibDot.read(f);
+    console.log("digraph", digraph)
+
     var nodeNames = digraph.nodes();
     var nodes = new Array(nodeNames.length);
     nodeNames.forEach(function (name, i) {
@@ -165,14 +197,13 @@ a = d3.text("graphdata.dot", function (f) {
         });
     }
 
-    console.log({
+    let arg = {
         nodes: nodes,
         links: edges
-    })
+    };
 
-    createPowerGraph({
-        nodes: nodes,
-        links: edges
-    });
+    console.log("arg", arg);
+
+    createPowerGraph(arg);
 });
 
